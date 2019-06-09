715f21739942684280fbea70db500c4bf213cf13
Thomas Hurst <tom@hur.st>
2019-06-09 23:03:24 +0100

    Fix alignment and factor out bit bashing

--- src/lib.rs
+++ src/lib.rs
@@ -53,11 +53,12 @@ pub struct BloomFilter {
 }
 
 const BLOOM_PAGE_SIZE: u32 = 1024 * 16;
+const BLOOM_PAGE_BIT_SIZE: u32 = BLOOM_PAGE_SIZE * 8;
 
 impl BloomFilter {
     pub fn from_params(mut params: BloomFilterParams) -> Self {
-        params.m = (params.m % (BLOOM_PAGE_SIZE * 8)) + params.m;
-        let pages = params.m / (BLOOM_PAGE_SIZE * 8);
+        params.m += BLOOM_PAGE_BIT_SIZE - (params.m % BLOOM_PAGE_BIT_SIZE);
+        let pages = params.m / BLOOM_PAGE_BIT_SIZE;
 
         Self {
             dirty: BitVec::from_elem(pages as usize, false),
@@ -72,62 +73,61 @@ impl BloomFilter {
         Self::from_params(BloomFilterParams::with_capacity_p(capacity, p))
     }
 
-    pub fn contains<T: Into<BloomHash>>(&self, item: T) -> bool {
-        let hash = item.into();
-
-        let offset = if self.pages > 0 {
-            (hash.nth(self.params.k + 1) % u64::from(self.pages)) * u64::from(BLOOM_PAGE_SIZE * 8)
-        } else {
-            0
-        };
-
-        for k in 0..self.params.k {
-            let bit = offset + (hash.nth(k) % u64::from(BLOOM_PAGE_SIZE));
-
-            if !self.filter.get(bit as usize).expect("within bounds") {
-                return false;
-            }
-        }
-
-        true
+    pub fn contains<T: Into<BloomHash>>(&mut self, item: T) -> bool {
+        self.check_or_insert(item.into(), false)
     }
 
     pub fn insert<T: Into<BloomHash>>(&mut self, item: T) -> bool {
-        let hash = item.into();
+        self.check_or_insert(item.into(), true)
+    }
 
+    fn check_or_insert(&mut self, hash: BloomHash, insert: bool) -> bool {
         let page = if self.pages > 0 {
             (hash.nth(self.params.k + 1) % u64::from(self.pages))
         } else {
             0
         };
 
-        let offset = page * u64::from(BLOOM_PAGE_SIZE * 8);
+        let offset = page * u64::from(BLOOM_PAGE_BIT_SIZE);
+
+        assert!(offset + u64::from(BLOOM_PAGE_BIT_SIZE) <= self.filter.len() as u64);
 
         let mut added = false;
 
         for k in 0..self.params.k {
-            let bit = offset + (hash.nth(k) % u64::from(BLOOM_PAGE_SIZE));
+            let bit = offset + (hash.nth(k) % u64::from(BLOOM_PAGE_BIT_SIZE));
 
             if !self.filter.get(bit as usize).expect("within bounds") {
+                if !insert {
+                    return false;
+                }
+
                 added = true;
 
                 self.filter.set(bit as usize, true);
             }
         }
 
+        if !insert {
+            return true;
+        }
+
         if added {
-            self.dirty.set(page as usize, true)
+            self.count += 1;
+            self.dirty.set(page as usize, true);
         }
 
         added
     }
 
-    /*
     pub fn count_estimate(&self) -> u32 {
         -((f64::from(self.params.m) / f64::from(self.params.k))
-            * (1.0 - (self.filter.count_ones() as f64 / f64::from(self.params.m)))) as u32
+            * (1.0 - (f64::from(self.count_ones()) / f64::from(self.params.m)))) as u32
     }
-    */
+
+    fn count_ones(&self) -> u32 {
+        self.filter.as_bytes().iter().map(|b| b.count_ones()).sum()
+    }
 
     pub fn is_full(&self) -> bool {
         self.count >= self.params.n
@@ -281,7 +281,7 @@ mod tests {
 
     #[test]
     fn bloomfilter_looks_reasonable() {
-        let mut bf = BloomFilter::with_capacity_p(400_000, 1e-9);
+        let mut bf = BloomFilter::with_capacity_p(400, 0.01);
 
         assert!(false == bf.contains("meep"));
         assert!(false == bf.contains(BloomHash::from("meep")));
